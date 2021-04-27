037d148838f16454ae04a138535fa17e25e44ad4
Ilayda <ilayda.bakare@gmail.com>
2021-04-27 15:26:58 +0100

    Completed quiz challenge app

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,8 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question(String q, bool a) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
+  // if you have {}, e.g. ({this.questionText, this.questionAnswer}) your questions HAVE
+  // to have look like this:
+  // Question(questionText: 'Some cats are actually allergic to humans', questionAnswer: true)
 }
